d6ab314971a440da188c22a12a6c3b42996075e3
MCeleste9 <137343523+MCeleste9@users.noreply.github.com>
2023-07-30 23:25:16 -0600

    Add files via upload

---
+++ Proyecto/Administadores/js/login.js
@@ -0,0 +1,30 @@
+/*async function  inicioSesion(){
+
+    const correo = document.getElementById("email").value;
+    const contrasena = document.getElementById("password").value;
+
+    let baseUrl = `http://localhost:3000/login`;
+    let data = {
+        usuario: correo,
+        contrasena: contrasena
+    }
+
+    let res = await fetch(baseUrl, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        
+    });
+    let dataResponse = await res.json();
+
+    if(dataResponse.exito){
+        localStorage.setItem('usuario', JSON.stringify(dataResponse.usuario));
+        window.location.href = 'home.html';
+    }else {
+        alert(dataResponse.mensaje);
+    }
+
+
+}
